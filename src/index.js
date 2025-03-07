1e777e0562b818dcee0f1df3f99bfc53aff98e56
Alex Frasca <alexander.frasca@student.swic.edu>
2025-03-07 16:40:12 -0600

    add callback functions

--- src/index.js
+++ src/index.js
@@ -7,3 +7,23 @@ const calcOrderCost1 = (order) => {
 }
 
 const calcOrderCost2 = (order) => order.quantity * order.priceEa
+
+function doSomethingWithNumber(number, callbackFunction) {
+    return callbackFunction(number)
+}
+
+function divideByTwo(num) {
+    return num / 2
+}
+
+function double(num) {
+    return num * 2
+}
+
+function triple(num) {
+    return num * 3
+}
+
+console.log(doSomethingWithNumber(42, double))
+console.log(doSomethingWithNumber(42, triple))
+console.log(doSomethingWithNumber(42, divideByTwo))
