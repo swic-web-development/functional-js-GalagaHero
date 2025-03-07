4f085ff96f9862540f00d1ceb27bdfa56efc158f
Alex Frasca <alexander.frasca@student.swic.edu>
2025-03-07 16:46:01 -0600

    add imperative loop and map method to compare the two

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-function calcOrderCost(order) {
+function calcOrderCost1(order) {
     return order.quantity * order.priceEa
 }
 
-const calcOrderCost1 = (order) => {
+const calcOrderCost2 = (order) => {
     return order.quantity * order.priceEa
 }
 
-const calcOrderCost2 = (order) => order.quantity * order.priceEa
+const calcOrderCost3 = (order) => order.quantity * order.priceEa
 
 function doSomethingWithNumber(number, callbackFunction) {
     return callbackFunction(number)
@@ -27,3 +27,22 @@ function triple(num) {
 console.log(doSomethingWithNumber(42, double))
 console.log(doSomethingWithNumber(42, triple))
 console.log(doSomethingWithNumber(42, divideByTwo))
+
+// imperative loop
+const numbers = [1, 2, 3, 4, 5]
+const doubledNumbers1 = []
+
+for (let i = 0; i < numbers.length; i++) {
+    doubledNumbers1.push(numbers[i] * 2)
+}
+
+console.log(doubledNumbers1)
+
+// `map` method
+const numbers2 = [1,2,3,4,5]
+
+const doubledNumbers2 = numbers.map(function (number) {
+    return number * 2
+})
+
+console.log(doubledNumbers2)
