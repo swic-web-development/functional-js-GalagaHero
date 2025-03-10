5e25fa0199b4c1c67e67bef8b30273571f253742
Alex Frasca <alexander.frasca@student.swic.edu>
2025-03-09 19:44:40 -0500

    add filter method demo

---
+++ src/arrays-practice.js
@@ -0,0 +1,15 @@
+
+const players = [
+  { name: 'DoomGuy', frags: 25, deaths: 0 },
+  { name: 'Slayer', frags: 25, deaths: 1 },
+  { name: 'Marine', frags: 12, deaths: 15 },
+  { name: 'Noob', frags: 0, deaths: 25 },
+]
+
+const playsWithMoreFragsThanDeaths = players.filter((player) => {
+    console.log(`We are now looking at this player: ${player.name}`)
+
+    return player.frags > player.deaths
+})
+
+console.log(playsWithMoreFragsThanDeaths)
