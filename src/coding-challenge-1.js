00c0095c2fbe8a7e14a4746d3f690015c4e5ed32
Alex Frasca <alexander.frasca@student.swic.edu>
2025-03-09 23:45:53 -0500

    add array and task

---
+++ src/coding-challenge-1.js
@@ -0,0 +1,9 @@
+// Backpack inventory for Coding Challenge 1
+const backpackFullOfAmmo = [
+    { item: 'bullets', quantity: 10, priceEa: 0.5 },
+    { item: 'shotgun shells', quantity: 4, priceEa: 0.5 },
+    { item: 'rockets', quantity: 1, priceEa: 5.0 },
+    { item: 'energy cell units', quantity: 20, priceEa: 0.2 },
+  ];
+
+  console.log('Task: How would we calculate the total value of the backpack?')
